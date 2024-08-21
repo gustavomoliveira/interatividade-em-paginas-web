099e2167d1dffde8f457dc5232c1d748784148da
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-08-21 13:44:32 +0200

    Add email validation with regex function and  <p> for messages

--- exercicios_event_listener/ex08/script.js
+++ exercicios_event_listener/ex08/script.js
@@ -8,15 +8,30 @@
 	4.	Mostre uma mensagem de erro ou sucesso dependendo da validação.
 */
 
-let inputName = document.getElementById('name').value;
-let inputEmail = document.getElementById('email').value;
-let inputPassword = document.getElementById('password').value;
 let buttonEnviar = document.querySelector('button');
+let message = document.getElementById('message');
 
-buttonEnviar.addEventListener('click', function() {
-    if(inputName !== '' && inputPassword !== '' && inputEmail !== '') {
-        alert('Sucesso no preenchimento do formulário!');
-    } else {
-        alert('Verifique os dados e tente novamente.');
-    }
+buttonEnviar.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	let inputName = document.getElementById('name').value.trim(); //o metodo trim() garante que não haverá espaços em branco a/d do preenchimento;
+	let inputEmail = document.getElementById('email').value.trim();
+	let inputPassword = document.getElementById('password').value.trim();
+
+    if(inputName === '' || inputPassword === '' || inputEmail === '') {
+		message.textContent = 'Verifique o preenchimento dos campos e tente novamente.'
+		message.style.color = 'darkred'
+	} else if(!validateEmail(inputEmail)) {
+		message.textContent = 'Dados de e-mail incorretos. Tente novamente.'
+		message.style.color = 'darkred'
+	} else {
+		message.textContent = 'Dados enviados com sucesso!'
+		message.style.color = 'green'
+	}
 });
+
+function validateEmail(email) { //função regex para validação de email;
+	const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return emailPattern.test(email);
+}
+	
